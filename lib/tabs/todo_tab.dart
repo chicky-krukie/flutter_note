b98a04ab34471a1117a712cff5a8b26686c7ffd2
neko-li <erica.flores0307@gmail.com>
2022-12-03 11:39:15 +0800

    added content on todo_tab

--- lib/tabs/todo_tab.dart
+++ lib/tabs/todo_tab.dart
@@ -1,16 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note/utils/todo_dialog.dart';
+import 'package:flutter_note/utils/todo_tile.dart';
 
-class TodoTab extends StatelessWidget {
+class TodoTab extends StatefulWidget {
   const TodoTab({super.key});
 
   @override
+  State<TodoTab> createState() => _TodoTabState();
+}
+
+class _TodoTabState extends State<TodoTab> {
+// text controller
+  final _controller = TextEditingController();
+
+// List of todo task
+  List todoList = [
+    ['Task 1', false],
+    ['Task 2', false],
+  ];
+// check was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
+
+// save new task
+  void saveNewTask() {
+    setState(() {
+      todoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+// create new task
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return TodoDialog(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
+      },
+    );
+  }
+
+// delete task
+  void deleteTask(int index) {
+    setState(() {
+      todoList.removeAt(index);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text(
-          'Add Todo Here',
-          style: TextStyle(color: Colors.grey)
-          ),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Scaffold(
+        body: ListView.builder(
+            itemCount: todoList.length,
+            itemBuilder: ((context, index) {
+              return TodoTile(
+                taskName: todoList[index][0],
+                taskCompleted: todoList[index][1],
+                onChanged: (value) => checkBoxChanged(value, index),
+                deleteFunction: (context) => deleteTask(index),
+              );
+            })),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            createNewTask();
+          },
+          child: const Icon(Icons.add, color: Colors.white),
+        ),
       ),
     );
   }
